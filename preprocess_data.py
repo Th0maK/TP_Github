da77d0d025a44bce233c57260ece2ce8831c2af6
benjamin333sz <b3szurek@gmail.com>
2025-11-07 10:00:50 +0100

    Changement fichier preprocess_data

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,7 +3,8 @@ from sklearn.model_selection import train_test_split
 def preprocess_data(data, testSize):
     """Nettoie, met en forme les données et prépare les ensembles de train et 
     de test"""
-    train, test = train_test_split(data, test_size = testSize)
+    # Nous mélangeons et prenons la sid 42
+    train, test = train_test_split(data, test_size = testSize, random_state=42,shuffle=True)
     return train, test
 
 
